8853a5e78184e1011b931d10ee983eb632fc640d
Maksud <bmaksud41@gmail.com>
2021-11-17 02:03:58 +0530

    Progress Bar Implemented

--- app/src/main/java/com/example/myrasdemo/EditProfile.java
+++ app/src/main/java/com/example/myrasdemo/EditProfile.java
@@ -2,10 +2,13 @@ package com.example.myrasdemo;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +22,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -43,6 +47,8 @@ public class EditProfile extends AppCompatActivity {
     FirebaseStorage storage;
     private DatabaseReference reference = FirebaseDatabase.getInstance().getReference("user_M");
     String str_first_name, str_last_name, str_profile_image, str_phone_no1, str_phone_no2, str_email, str_password, str_licence_no, str_address_proof_no, str_address, str_area, str_city, str_state, str_pincode;
+    ProgressDialog progressDialog;
+    private static int timeOut=500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -118,6 +124,7 @@ public class EditProfile extends AppCompatActivity {
                                 reference.child(str_phone_no1).child("profile_image").setValue(uri.toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
                                     @Override
                                     public void onSuccess(Void unused) {
+                                        progressDialog.dismiss();
                                     }
                                 });
                             }
@@ -131,6 +138,10 @@ public class EditProfile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 launcher.launch("image/*");
+                progressDialog = new ProgressDialog(EditProfile.this);
+                progressDialog.show();
+                progressDialog.setContentView(R.layout.progress_dialog);
+                progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
             }
         });
 
@@ -147,63 +158,72 @@ public class EditProfile extends AppCompatActivity {
         updatebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reference.child(str_phone_no1).child("first_name").setValue(first_name.getText().toString());
-                reference.child(str_phone_no1).child("last_name").setValue(last_name.getText().toString());
-                reference.child(str_phone_no1).child("phoneno1").setValue(phone_no1.getText().toString());
-                reference.child(str_phone_no1).child("phoneno2").setValue(phone_no2.getText().toString());
-                reference.child(str_phone_no1).child("email").setValue(email.getText().toString());
-                reference.child(str_phone_no1).child("password").setValue(password.getText().toString());
-                reference.child(str_phone_no1).child("licence_no").setValue(license_no.getText().toString());
-                reference.child(str_phone_no1).child("address_proof_no").setValue(address_proof_no.getText().toString());
-                reference.child(str_phone_no1).child("address").setValue(address.getText().toString());
-                reference.child(str_phone_no1).child("area").setValue(area.getText().toString());
-                reference.child(str_phone_no1).child("city").setValue(city.getText().toString());
-                reference.child(str_phone_no1).child("state").setValue(state.getText().toString());
-                reference.child(str_phone_no1).child("pincode").setValue(pincode.getText().toString());
-                Query checkUser = reference.orderByChild("phoneno1").equalTo(str_phone_no1);
-                checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                progressDialog = new ProgressDialog(EditProfile.this);
+                progressDialog.show();
+                progressDialog.setContentView(R.layout.progress_dialog);
+                progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                new Handler().postDelayed(new Runnable() {
                     @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if (snapshot.exists())
-                        {
-                            String str_password = snapshot.child(str_phone_no1).child("password").getValue(String.class);
-                            String str_first_name = snapshot.child(str_phone_no1).child("first_name").getValue(String.class);
-                            String str_last_name = snapshot.child(str_phone_no1).child("last_name").getValue(String.class);
-                            String str_profile_image = snapshot.child(str_phone_no1).child("profile_image").getValue(String.class);
-                            String str_phone_no2 = snapshot.child(str_phone_no1).child("phoneno2").getValue(String.class);
-                            String str_email = snapshot.child(str_phone_no1).child("email").getValue(String.class);
-                            String str_licence_no = snapshot.child(str_phone_no1).child("licence_no").getValue(String.class);
-                            String str_address_proof_no = snapshot.child(str_phone_no1).child("address_proof_no").getValue(String.class);
-                            String str_address = snapshot.child(str_phone_no1).child("address").getValue(String.class);
-                            String str_area = snapshot.child(str_phone_no1).child("area").getValue(String.class);
-                            String str_city = snapshot.child(str_phone_no1).child("city").getValue(String.class);
-                            String str_state = snapshot.child(str_phone_no1).child("state").getValue(String.class);
-                            String str_pincode = snapshot.child(str_phone_no1).child("pincode").getValue(String.class);
-                            Toast.makeText(EditProfile.this,"Profile Successfully Updated",Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(EditProfile.this,MainActivity.class);
-                            i.putExtra("first_name",str_first_name);
-                            i.putExtra("last_name",str_last_name);
-                            i.putExtra("profile_image",str_profile_image);
-                            i.putExtra("phoneno1",str_phone_no1);
-                            i.putExtra("phoneno2",str_phone_no2);
-                            i.putExtra("email",str_email);
-                            i.putExtra("password",str_password);
-                            i.putExtra("licence_no",str_licence_no);
-                            i.putExtra("address_proof_no",str_address_proof_no);
-                            i.putExtra("address",str_address);
-                            i.putExtra("area",str_area);
-                            i.putExtra("city",str_city);
-                            i.putExtra("state",str_state);
-                            i.putExtra("pincode",str_pincode);
-                            startActivity(i);
-                            finish();
-                        }
-                    }
+                    public void run() {
+                        reference.child(str_phone_no1).child("first_name").setValue(first_name.getText().toString());
+                        reference.child(str_phone_no1).child("last_name").setValue(last_name.getText().toString());
+                        reference.child(str_phone_no1).child("phoneno1").setValue(phone_no1.getText().toString());
+                        reference.child(str_phone_no1).child("phoneno2").setValue(phone_no2.getText().toString());
+                        reference.child(str_phone_no1).child("email").setValue(email.getText().toString());
+                        reference.child(str_phone_no1).child("password").setValue(password.getText().toString());
+                        reference.child(str_phone_no1).child("licence_no").setValue(license_no.getText().toString());
+                        reference.child(str_phone_no1).child("address_proof_no").setValue(address_proof_no.getText().toString());
+                        reference.child(str_phone_no1).child("address").setValue(address.getText().toString());
+                        reference.child(str_phone_no1).child("area").setValue(area.getText().toString());
+                        reference.child(str_phone_no1).child("city").setValue(city.getText().toString());
+                        reference.child(str_phone_no1).child("state").setValue(state.getText().toString());
+                        reference.child(str_phone_no1).child("pincode").setValue(pincode.getText().toString());
+                        Query checkUser = reference.orderByChild("phoneno1").equalTo(str_phone_no1);
+                        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists())
+                                {
+                                    String str_password = snapshot.child(str_phone_no1).child("password").getValue(String.class);
+                                    String str_first_name = snapshot.child(str_phone_no1).child("first_name").getValue(String.class);
+                                    String str_last_name = snapshot.child(str_phone_no1).child("last_name").getValue(String.class);
+                                    String str_profile_image = snapshot.child(str_phone_no1).child("profile_image").getValue(String.class);
+                                    String str_phone_no2 = snapshot.child(str_phone_no1).child("phoneno2").getValue(String.class);
+                                    String str_email = snapshot.child(str_phone_no1).child("email").getValue(String.class);
+                                    String str_licence_no = snapshot.child(str_phone_no1).child("licence_no").getValue(String.class);
+                                    String str_address_proof_no = snapshot.child(str_phone_no1).child("address_proof_no").getValue(String.class);
+                                    String str_address = snapshot.child(str_phone_no1).child("address").getValue(String.class);
+                                    String str_area = snapshot.child(str_phone_no1).child("area").getValue(String.class);
+                                    String str_city = snapshot.child(str_phone_no1).child("city").getValue(String.class);
+                                    String str_state = snapshot.child(str_phone_no1).child("state").getValue(String.class);
+                                    String str_pincode = snapshot.child(str_phone_no1).child("pincode").getValue(String.class);
+                                    Toast.makeText(EditProfile.this,"Profile Successfully Updated",Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(EditProfile.this,MainActivity.class);
+                                    i.putExtra("first_name",str_first_name);
+                                    i.putExtra("last_name",str_last_name);
+                                    i.putExtra("profile_image",str_profile_image);
+                                    i.putExtra("phoneno1",str_phone_no1);
+                                    i.putExtra("phoneno2",str_phone_no2);
+                                    i.putExtra("email",str_email);
+                                    i.putExtra("password",str_password);
+                                    i.putExtra("licence_no",str_licence_no);
+                                    i.putExtra("address_proof_no",str_address_proof_no);
+                                    i.putExtra("address",str_address);
+                                    i.putExtra("area",str_area);
+                                    i.putExtra("city",str_city);
+                                    i.putExtra("state",str_state);
+                                    i.putExtra("pincode",str_pincode);
+                                    startActivity(i);
+                                    finish();
+                                }
+                            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                            }
+                        });
                     }
-                });
+                },timeOut);
             }
         });
     }
@@ -219,6 +239,12 @@ public class EditProfile extends AppCompatActivity {
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                SharedPreferences sp = getSharedPreferences("credentials",MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear();
+                editor.apply();
+                Task<Void> reference;
+                reference = FirebaseDatabase.getInstance().getReference().child("user_M").child(str_phone_no1).setValue(null);
                 Toast.makeText(activity.getApplicationContext(), "Profile Deactivated Successfully",Toast.LENGTH_SHORT).show();
                 Intent i = new Intent(EditProfile.this,Login.class);
                 startActivity(i);
