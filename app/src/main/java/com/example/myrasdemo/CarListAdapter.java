7602f5cf47df031d427e20bf827efb48e06439c3
Maksud <bmaksud41@gmail.com>
2021-11-17 20:38:48 +0530

    Car Info Sent To Booking Details Page

--- app/src/main/java/com/example/myrasdemo/CarListAdapter.java
+++ app/src/main/java/com/example/myrasdemo/CarListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myrasdemo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,19 +18,27 @@ import java.util.ArrayList;
 
 public class CarListAdapter extends RecyclerView.Adapter<CarListAdapter.MyViewHolder>{
 
-    Context context;
+    Context contextActivity;
+    Context contextCar;
     ArrayList<CarHelperClass> list;
+    private  String str_start_date, str_start_time, str_end_date, str_end_time;
 
-    public CarListAdapter(Context context, ArrayList<CarHelperClass> list) {
-        this.context = context;
+    public CarListAdapter(Context contextActivity, ArrayList<CarHelperClass> list, Context contextCar, String start_date, String start_time, String end_date, String end_time) {
+//        public CarListAdapter(Context contextActivity, ArrayList<CarHelperClass> list, Context contextCar) {
+        this.contextActivity = contextActivity;
+        this.contextCar = contextCar;
         this.list = list;
+        this.str_start_date = start_date;
+        this.str_start_time = start_time;
+        this.str_end_date = end_date;
+        this.str_end_time = end_time;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View v = LayoutInflater.from(context).inflate(R.layout.cars_list,parent,false);
+        View v = LayoutInflater.from(contextActivity).inflate(R.layout.cars_list,parent,false);
         return new MyViewHolder(v);
 
     }
@@ -37,12 +46,97 @@ public class CarListAdapter extends RecyclerView.Adapter<CarListAdapter.MyViewHo
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
-        CarHelperClass carHelperClass = list.get(position);
-        holder.car_name.setText(carHelperClass.getCar_name());
-        holder.seat_capacity.setText(carHelperClass.getSeat_capacity());
-        holder.fuel_type.setText(carHelperClass.getFuel_type());
-        holder.transmission_type.setText(carHelperClass.getTransmission_type());
-        Picasso.get().load(carHelperClass.getCar_image()).into(holder.car_image);
+            String str_rent_price, str_small_trip_fare_price, str_medium_trip_fare_price, str_high_trip_fare_price;
+            Integer int_small_package_kms=150, int_medium_package_kms=350, int_high_package_kms=600, int_rent_price, int_small_trip_fare_price, int_medium_trip_fare_price, int_high_trip_fare_price;
+            CarHelperClass carHelperClass = list.get(position);
+            holder.car_name.setText(carHelperClass.getCar_name());
+            holder.seat_capacity.setText(carHelperClass.getSeat_capacity());
+            holder.fuel_type.setText(carHelperClass.getFuel_type());
+            holder.transmission_type.setText(carHelperClass.getTransmission_type());
+            Picasso.get().load(carHelperClass.getCar_image()).into(holder.car_image);
+            str_rent_price = carHelperClass.getRent_price();
+            int_rent_price = Integer.parseInt(str_rent_price);
+            int_small_trip_fare_price = int_small_package_kms*int_rent_price;
+            str_small_trip_fare_price = int_small_trip_fare_price.toString();
+            holder.smallpackagebtn.setText("Rs "+str_small_trip_fare_price);
+            holder.smallpackagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str_kms = "150";
+                Intent intent = new Intent(contextCar,BookingDetails.class);
+                intent.putExtra("body_type",carHelperClass.getBody_type());
+                intent.putExtra("car_image",carHelperClass.getCar_image());
+                intent.putExtra("car_name",carHelperClass.getCar_name());
+                intent.putExtra("car_no_plate",carHelperClass.getCar_no_plate());
+                intent.putExtra("fuel_type",carHelperClass.getFuel_type());
+                intent.putExtra("car_name",carHelperClass.getCar_name());
+                intent.putExtra("rent_price",carHelperClass.getRent_price());
+                intent.putExtra("seat_capacity",carHelperClass.getSeat_capacity());
+                intent.putExtra("transmission_type",carHelperClass.getTransmission_type());
+                intent.putExtra("kms",str_kms);
+                intent.putExtra("start_date",str_start_date);
+                intent.putExtra("start_time",str_start_time);
+                intent.putExtra("end_date",str_end_date);
+                intent.putExtra("end_time",str_end_time);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                contextCar.startActivity(intent);
+            }
+        });
+
+        int_medium_trip_fare_price = int_medium_package_kms*int_rent_price;
+        str_medium_trip_fare_price = int_medium_trip_fare_price.toString();
+        holder.mediumpackagebtn.setText("Rs "+str_medium_trip_fare_price);
+        holder.mediumpackagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str_kms = "350";
+                Intent intent = new Intent(contextCar,BookingDetails.class);
+                intent.putExtra("body_type",carHelperClass.getBody_type());
+                intent.putExtra("car_image",carHelperClass.getCar_image());
+                intent.putExtra("car_name",carHelperClass.getCar_name());
+                intent.putExtra("car_no_plate",carHelperClass.getCar_no_plate());
+                intent.putExtra("fuel_type",carHelperClass.getFuel_type());
+                intent.putExtra("car_name",carHelperClass.getCar_name());
+                intent.putExtra("rent_price",carHelperClass.getRent_price());
+                intent.putExtra("seat_capacity",carHelperClass.getSeat_capacity());
+                intent.putExtra("transmission_type",carHelperClass.getTransmission_type());
+                intent.putExtra("kms",str_kms);
+                intent.putExtra("start_date",str_start_date);
+                intent.putExtra("start_time",str_start_time);
+                intent.putExtra("end_date",str_end_date);
+                intent.putExtra("end_time",str_end_time);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                contextCar.startActivity(intent);
+
+            }
+        });
+
+        int_high_trip_fare_price = int_high_package_kms*int_rent_price;
+        str_high_trip_fare_price = int_high_trip_fare_price.toString();
+        holder.highpackagebtn.setText("Rs "+str_high_trip_fare_price);
+        holder.highpackagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str_kms = "600";
+                Intent intent = new Intent(contextCar,BookingDetails.class);
+                intent.putExtra("body_type",carHelperClass.getBody_type());
+                intent.putExtra("car_image",carHelperClass.getCar_image());
+                intent.putExtra("car_name",carHelperClass.getCar_name());
+                intent.putExtra("car_no_plate",carHelperClass.getCar_no_plate());
+                intent.putExtra("fuel_type",carHelperClass.getFuel_type());
+                intent.putExtra("car_name",carHelperClass.getCar_name());
+                intent.putExtra("rent_price",carHelperClass.getRent_price());
+                intent.putExtra("seat_capacity",carHelperClass.getSeat_capacity());
+                intent.putExtra("transmission_type",carHelperClass.getTransmission_type());
+                intent.putExtra("kms",str_kms);
+                intent.putExtra("start_date",str_start_date);
+                intent.putExtra("start_time",str_start_time);
+                intent.putExtra("end_date",str_end_date);
+                intent.putExtra("end_time",str_end_time);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                contextCar.startActivity(intent);
+            }
+        });
 
     }
 
@@ -59,6 +153,7 @@ public class CarListAdapter extends RecyclerView.Adapter<CarListAdapter.MyViewHo
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
             car_image = itemView.findViewById(R.id.carImage);
             car_name = itemView.findViewById(R.id.carName);
             seat_capacity = itemView.findViewById(R.id.carSeat);
--- app/src/main/java/com/example/myrasdemo/MainActivity.java
+++ app/src/main/java/com/example/myrasdemo/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference database;
     CarListAdapter carListAdapter;
     ArrayList<CarHelperClass> list;
+    private String str_start_date, str_start_time, str_end_date, str_end_time;
     private static int timeOut=500;
     ProgressDialog progressDialog;
     private String str_first_name, str_last_name, str_full_name, str_profile_image, str_phone_no1, str_phone_no2, str_email, str_password, str_licence_no, str_address_proof_no, str_address, str_area, str_city, str_state, str_pincode;
@@ -141,8 +142,10 @@ public class MainActivity extends AppCompatActivity {
                         calendar.set(Calendar.MINUTE,minute);
                         SimpleDateFormat startDateFormat = new SimpleDateFormat("MMMM  dd  yyyy");
                         startDate.setText(startDateFormat.format(calendar.getTime()));
+                        str_start_date = startDateFormat.format(calendar.getTime());
                         SimpleDateFormat starttimeFormat = new SimpleDateFormat("EEEE HH:mm");
                         startTime.setText(starttimeFormat.format(calendar.getTime()));
+                        str_start_time = starttimeFormat.format(calendar.getTime());
                     }
                 };
                 new TimePickerDialog(MainActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
@@ -167,8 +170,10 @@ public class MainActivity extends AppCompatActivity {
                         calendar.set(Calendar.MINUTE,minute);
                         SimpleDateFormat endDateFormat = new SimpleDateFormat("MMMM  dd  yyyy");
                         endDate.setText(endDateFormat.format(calendar.getTime()));
+                        str_end_date = endDateFormat.format(calendar.getTime());
                         SimpleDateFormat endtimeFormat = new SimpleDateFormat("EEEE HH:mm");
                         endTime.setText(endtimeFormat.format(calendar.getTime()));
+                        str_end_time = endtimeFormat.format(calendar.getTime());
                     }
                 };
                 new TimePickerDialog(MainActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
@@ -182,7 +187,8 @@ public class MainActivity extends AppCompatActivity {
         carlistrecyclerView.setHasFixedSize(true);
         carlistrecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         list = new ArrayList<>();
-        carListAdapter = new CarListAdapter(MainActivity.this,list);
+//        carListAdapter = new CarListAdapter(MainActivity.this,list,getApplicationContext());
+        carListAdapter = new CarListAdapter(MainActivity.this,list,getApplicationContext(),str_start_date,str_start_time,str_end_date,str_end_time);
         carlistrecyclerView.setAdapter(carListAdapter);
         database.addValueEventListener(new ValueEventListener() {
             @Override
