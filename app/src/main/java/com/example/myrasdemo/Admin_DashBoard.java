624a1decfebcfba92e21eb0889d246ec2771a592
Maksud <bmaksud41@gmail.com>
2021-11-18 13:11:07 +0530

    AddCarFrom

---
+++ app/src/main/java/com/example/myrasdemo/Admin_DashBoard.java
@@ -0,0 +1,14 @@
+package com.example.myrasdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_DashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dash_board);
+    }
+}
